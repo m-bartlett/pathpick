f7c09e13ed5bf55bed331423f96ae961b1e47969
M B <85039141+m-bartlett@users.noreply.github.com>
2021-11-03 02:12:11 -0600

    split modules since makefile will compile
    plan to use pyinstaller to 'compile' to one file
    add pageup/pagedown cursor movement

---
+++ interactive_terminal_application.py
@@ -0,0 +1,88 @@
+import termios, atexit, sys, os, signal
+
+"""
+https://invisible-island.net/xterm/ctlseqs/ctlseqs.html
+ESC [    Control Sequence Introducer (CSI  is 0x9b).
+"""
+
+def singleton(cls):
+  instances = {}
+  def getinstance(*args, **kwargs):
+    return instances.get(cls, cls(*args, **kwargs))
+  return getinstance
+
+
+class InteractiveTerminalApplication():
+  fd, stty = None, None
+  WIDTH, HEIGHT = 10, 10
+
+  @staticmethod
+  def puts(s):
+    print(s, end='')
+    sys.stdout.flush()
+
+
+  def resize(self, *args):
+    self.WIDTH, self.HEIGHT = os.get_terminal_size()
+
+
+  def __init__(self):
+    self.fd = sys.stdin.fileno()
+    self.stty = termios.tcgetattr(self.fd)  # save current TTY settings
+
+
+  def close(self):
+    termios.tcsetattr(self.fd, termios.TCSADRAIN, self.stty) # restore saved TTY settings, e.g. echo & icanon
+    self.puts(
+      # "\033[2J"                # Clear screen
+      "\033[?25h"              # Show cursor
+      "\033[?1004l"            # Disable focus-in/out reporting method 1
+      "\033]777;focus;off\x07" # Disable focus-in/out reporting method 2 (urxvt)
+    )
+    from time import sleep
+    sys.stderr.flush()
+    sys.stdout.flush()
+    sleep(5)
+    self.puts(
+      "\033[?1049l"            # Switch back to primary screen buffer
+    )
+
+
+  def launch(self):
+    """ Manual terminal graphics init """
+    atexit.register(self.close) # in case an unexpected exit occurs, restore the terminal back to its starting state
+    new = termios.tcgetattr(self.fd)       #
+    new[3] = new[3] & ~termios.ECHO   # disable echo, i.e. don't print input chars
+    new[3] = new[3] & ~termios.ICANON # disable canonical (line edit) mode, chars sent immediately without buffer
+      # *T*erminal *C*hange *S*hell *A*ction
+      # TCSANOW -> change occurs immediately
+      # TCSADRAIN -> await all output to be transmitted. Use when changing parameters that affect output.
+      # TCSAFLUSH -> await all output to be transmitted, and all existing unprocessed input is discarded.
+    termios.tcsetattr(self.fd, termios.TCSADRAIN, new)
+    self.puts(
+      "\033[?1004h"           # Enable focus-in/out reporting method 1
+      "\033]777;focus;on\x07" # Enable focus-in/out reporting method 2 (urxvt)
+      "\033[?25l"             # Hide cursor
+      "\033[?1049h"           # Switch to alternate screen buffer
+      "\033[2J"               # Clear entire screen
+    )
+    signal.signal(signal.SIGWINCH, self.resize)
+    self.resize()
+
+
+  def end(self, *args, return_code=1, throw=False):
+    atexit.unregister(self.close) # prevent duplicate execution of terminal restore
+    self.close()
+    signal.signal(signal.SIGWINCH, signal.SIG_DFL) # remove signal handler
+    self.__exit__ = lambda: None
+    if throw:
+      raise KeyboardInterrupt # received a user-input exit
+    return return_code
+
+
+  def __enter__(self):
+    self.launch()
+    return self
+
+
+  __exit__  = end
--- pathpick.py
+++ pathpick.py
@@ -1,93 +1,6 @@
 #!/usr/bin/env python
-import termios, atexit, sys, os, signal, pathlib
-from textwrap import shorten
-# from time import sleep
-
-"""
-https://invisible-island.net/xterm/ctlseqs/ctlseqs.html
-ESC [    Control Sequence Introducer (CSI  is 0x9b).
-"""
-
-def singleton(cls):
-  instances = {}
-  def getinstance(*args, **kwargs):
-    return instances.get(cls, cls(*args, **kwargs))
-  return getinstance
-
-
-class InteractiveTerminalApplication():
-  fd, stty = None, None
-  WIDTH, HEIGHT = 10, 10
-
-  @staticmethod
-  def puts(s):
-    print(s, end='')
-    sys.stdout.flush()
-
-  @staticmethod
-  def _do_nothing():
-    return
-
-
-  def resize(self, *args):
-    self.WIDTH, self.HEIGHT = os.get_terminal_size()
-
-
-  def __init__(self):
-    self.fd = sys.stdin.fileno()
-    self.stty = termios.tcgetattr(self.fd)  # save current TTY settings
-
-
-  def close(self):
-    termios.tcsetattr(self.fd, termios.TCSADRAIN, self.stty) # restore saved TTY settings, e.g. echo & icanon
-    self.puts(
-      # "\033[2J"                # Clear screen
-      "\033[?25h"              # Show cursor
-      "\033[?1004l"            # Disable focus-in/out reporting method 1
-      "\033]777;focus;off\x07" # Disable focus-in/out reporting method 2 (urxvt)
-      "\033[?1049l"            # Switch back to primary screen buffer
-    )
-
-
-  def launch(self):
-    """ Manual terminal graphics init """
-    atexit.register(self.close) # in case an unexpected exit occurs, restore the terminal back to its starting state
-    new = termios.tcgetattr(self.fd)       #
-    new[3] = new[3] & ~termios.ECHO   # disable echo, i.e. don't print input chars
-    new[3] = new[3] & ~termios.ICANON # disable canonical (line edit) mode, chars sent immediately without buffer
-      # *T*erminal *C*hange *S*hell *A*ction
-      # TCSANOW -> change occurs immediately
-      # TCSADRAIN -> await all output to be transmitted. Use when changing parameters that affect output.
-      # TCSAFLUSH -> await all output to be transmitted, and all existing unprocessed input is discarded.
-    termios.tcsetattr(self.fd, termios.TCSADRAIN, new)
-    self.puts(
-      "\033[?1004h"           # Enable focus-in/out reporting method 1
-      "\033]777;focus;on\x07" # Enable focus-in/out reporting method 2 (urxvt)
-      "\033[?25l"             # Hide cursor
-      "\033[?1049h"           # Switch to alternate screen buffer
-      "\033[2J"               # Clear entire screen
-    )
-    signal.signal(signal.SIGWINCH, self.resize)
-    self.resize()
-
-
-  def end(self, *args, return_code=1, throw=False):
-    atexit.unregister(self.close)
-    self.close()
-    signal.signal(signal.SIGWINCH, signal.SIG_DFL) # remove signal handler
-    self.__exit__ = self._do_nothing
-    if throw:
-      raise KeyboardInterrupt
-    return return_code
-
-
-  def __enter__(self):
-    self.launch()
-    return self
-
-  __exit__  = end
-
-
+from interactive_terminal_application import *
+import pathlib
 
 
 @singleton
@@ -98,14 +11,7 @@ class InteractiveFilesystemPathSelector(InteractiveTerminalApplication):
   path_list = []
   path_list_last = 0
   index = 0
-
-  @staticmethod
-  def _glob2paths_no_hidden(glob):
-    return [
-      ( p.name + '/' if p.is_dir() else p.name )
-      for p in glob
-      if not p.name.startswith('.')
-    ]
+  FILE_PREFIX = '  '
 
   @staticmethod
   def _glob2paths_with_hidden(glob):
@@ -114,13 +20,19 @@ class InteractiveFilesystemPathSelector(InteractiveTerminalApplication):
       for p in glob
     ]
 
+  @classmethod
+  def _glob2paths_no_hidden(cls, glob):
+    return [
+      p for p in cls._glob2paths_with_hidden(glob)
+      if not p.startswith('.')
+    ]
+
   @staticmethod
   def _sort_path_list(glob):
     return sorted(glob)
 
   @staticmethod
   def _sort_path_list_directories_first(glob):
-      path_list = [i for i in glob]
       dirs = [i for i in glob if i.endswith('/')]
       files = [i for i in glob if not i.endswith('/')]
       return sorted(dirs) + sorted(files)
@@ -133,7 +45,8 @@ class InteractiveFilesystemPathSelector(InteractiveTerminalApplication):
                 dirs_first = False  ):
     super().__init__()
 
-    if root:
+    if root and root != '.':
+
       root = pathlib.Path(root).expanduser()
       if not root.exists(): raise FileNotFoundError(f"Path '{root}' does not exist")
       if not root.is_dir(): raise NotADirectoryError(f"Path '{root}' is not a directory")
@@ -174,17 +87,18 @@ class InteractiveFilesystemPathSelector(InteractiveTerminalApplication):
       # ord("O"): lambda: puts("unfocus"),
       ord("A"): self.cursor_up,          # up
       ord("B"): self.cursor_down,        # down
+      ord("5"): self.page_up,            # PageUp
+      ord("6"): self.page_down,          # PageDown
       ord("C"): self.select_or_descend,  # right
       ord("D"): self.ascend,             # left
       ord(' '): lambda: self.end(return_code=1, throw=True)  # escape
     }
-    
+
 
   def resize(self, *args):
     self.WIDTH, self.HEIGHT = os.get_terminal_size()
     self.HEIGHT_1 = self.HEIGHT - 1
-    self.HEIGHT_2 = self.HEIGHT - 2
-    self.HEIGHT_3 = self.HEIGHT - 3
+    self.draw_page()
 
 
   def cd(self, _dir):
@@ -211,6 +125,27 @@ class InteractiveFilesystemPathSelector(InteractiveTerminalApplication):
     try: self.character_action_map[char](event)
     except KeyError: pass
 
+    # try:
+    #   self.character_action_map.get(
+    #     char,
+    #     lambda e: self.puts(f"dicks")
+    #   )(event)
+    # except e:
+    #   raise self.puts(e)
+
+
+  def draw_header(self, page, pages, page_start, page_end, row):
+    header = f"  -  {page+1}/{pages+1} ({self.index*100//self.path_list_len}%)"
+    path_width = self.WIDTH - len(header)
+    path = str(self.cwd)
+    if len(path) > path_width:
+      path = '...' + path[-(path_width-3):]
+    self.puts(f"\033[37;1m{path}{header}\033[0m")
+
+
+  def constrain_width(self, s):
+    return s[:self.WIDTH-3]+'...' if len(s) > self.WIDTH else s
+
 
   def draw_page(self):
     self.puts(
@@ -218,14 +153,17 @@ class InteractiveFilesystemPathSelector(InteractiveTerminalApplication):
       "\033[2J"   # Clear entire screen
     )
     page, row = divmod(self.index, self.HEIGHT_1)
+    pages = self.path_list_len // self.HEIGHT_1
     row += 2
     page_start = page * self.HEIGHT_1
-    page_end = page_start + self.HEIGHT_1 # min(self.HEIGHT_2, self.path_list_len - page_start)
-    print(self.cwd, page, page_start, page_end, row, self.index)
-    prefix = '  '
-    self.puts(prefix)
-    self.puts( ('\n'+prefix).join([ shorten(str(i), self.WIDTH, placeholder='...')
-                                for i in self.path_list[page_start:page_end] ]) )
+    page_end = page_start + self.HEIGHT_1
+    self.draw_header(page, pages, page_start, page_end, row)
+    for p in self.path_list[page_start:page_end]:
+      self.puts('\n')
+      item_text = self.constrain_width(self.FILE_PREFIX + p)
+      self.puts(item_text)
+      # TO-DO highlight selected items
+      # \033[1m
     self.puts(f"\033[{row};0H>")
     sys.stdout.flush()
 
@@ -241,6 +179,20 @@ class InteractiveFilesystemPathSelector(InteractiveTerminalApplication):
     self.index += 1
     if self.index > self.path_list_last:
       self.index = 0
+    self.draw_page()
+
+
+  def page_up(self):
+    self.index -= self.HEIGHT_1
+    if self.index < 0:
+      self.index = self.path_list_last + self.index
+    self.draw_page()
+
+
+  def page_down(self):
+    self.index += self.HEIGHT_1
+    if self.index > self.path_list_last:
+      self.index =  self.index - self.path_list_last
     self.draw_page()
 
 
@@ -265,9 +217,9 @@ if __name__ == "__main__":
   import argparse
   parser = argparse.ArgumentParser()
   parser.add_argument("root", type=str, nargs='?', default=None, help="Directory to explore. Default is $PWD")
-  parser.add_argument("--absolute", action="store_true", help="Use absolute paths for selection display and output")
-  parser.add_argument("--hidden", action="store_true", help="Show hidden files and directories that are prefixed with '.'")
-  parser.add_argument("--dirs-first", action="store_true", help="")
+  parser.add_argument("--hidden", '-a', action="store_true", help="Show files and directories that start with '.'")
+  parser.add_argument("--absolute", '-b', action="store_true", help="Use absolute paths for selection display and output")
+  parser.add_argument("--dirs-first", '-d', action="store_true", help="")
   args = parser.parse_args()
 
   with InteractiveFilesystemPathSelector( root         = args.root,
